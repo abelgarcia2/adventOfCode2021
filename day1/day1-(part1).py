64bdf4c0fd7231f16a4d5f2a8fe13a9e484418fb
abelgarcia2 <65343232+abelgarcia2@users.noreply.github.com>
2021-12-01 14:57:37 +0100

    Day 1 (1/2)

---
+++ day1/day1-(part1).py
@@ -0,0 +1,14 @@
+f = open('day1\input.txt', 'r')
+
+strList = f.readlines()
+numList = list(map(int, strList))
+
+cont = 0
+aux = numList[0]
+
+for i in numList:
+    if (i > aux):
+        cont += 1
+    aux = i
+
+print(cont)
---
+++ day1/day1-(part2).py
@@ -0,0 +1,10 @@
+f = open('day1\input.txt', 'r')
+
+strList = f.readlines()
+numList = []
+
+for i in strList:
+    numList.append(int(i))
+
+for i in numList:
+    
